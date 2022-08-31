255c7769d172c4b69a651c3a0369d74d8bb875a5
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-08-31 17:00:19 -0400

    Refactorizando codigo

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -43,11 +43,35 @@
       return true;
   }
 
+  // 
+  const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
+    // inicio del codigo horroroso
+    // let result: number;
+    // if ( isDead ) {
+    //     result = 1500;
+    // } else {
+    //     if ( isSeparated ) {
+    //         result = 2500;
+    //     } else {
+    //         if ( isRetired ) {
+    //             result = 3000;
+    //         } else {
+    //             result = 4000; 
+    //         }
+    //     }
+    // } 
+    // return result;
+
+    // Codigo refactorizado
+    if ( isDead ) return 1500;  
+
+    if ( isSeparated ) return 2500;  
+
+    return ( isRetired ) ? 3000 : 4000;  
+
+  }
   
 
 
 })();
-
-
-
-
+ 
