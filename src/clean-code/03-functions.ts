458c6dfb792b102e7713183bdcfe60ebe84699d7
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-08-31 15:15:53 -0400

    Mejorando funciones para tener un Clean Code y ordenado

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,53 @@
+(() => {
+
+  // función para obtener información de una película por Id
+  function getMovieByMovieId( movieId: string ) {
+      console.log({ movieId });
+  }
+
+  // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+  function getAllActorsByMovieId( movieId: string ) {
+      console.log({ movieId });
+  }
+
+  // funcion para obtener el bio del actor por el id
+  function getActorBioByActorId( ActorId: string ) {
+      console.log({ ActorId });
+  }
+  
+  // Crear una película
+  interface Movie {
+    title:        string, 
+    description:  string, 
+    rating:       number, 
+    cast:         string[]
+  }
+  function createMovie({title, description, rating, cast}: Movie ) {
+      console.log({ title, description, rating, cast });
+  }
+
+  function checkFullName(fullName: string){
+    console.log({fullName});
+    return true;
+  }
+
+  // Crea un nuevo actor
+  function createActor( fullName: string, birthdate: Date ): boolean {
+      
+      // tarea asincrona para verificar nombre
+      // ..
+      // ..
+      if ( checkFullName(fullName) ) return false;
+
+      console.log('Crear actor', birthdate);
+      return true;
+  }
+
+  
+
+
+})();
+
+
+
+
