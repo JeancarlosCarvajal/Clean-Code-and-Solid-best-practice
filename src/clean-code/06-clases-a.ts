b6f12db6109a26f9d0630659b8675de637ff9d21
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-09-06 11:09:04 -0400

    Ejemplo de NOO Aplica el Principio de Responsabilidad Unica

---
+++ src/clean-code/06-clases-a.ts
@@ -0,0 +1,79 @@
+(()=>{
+
+  // No aplicando el principio de responsabilidad Unica
+  type Gender = 'M' | 'F';
+  class Person {
+    // opcion 1
+    // public name: string;
+    // public gender: Gender;
+    // public birthDay: Date;
+
+    // constructor(name: string, gender: Gender, birthDay: Date){
+    //   this.name = name;
+    //   this.gender = gender;
+    //   this.birthDay = birthDay;
+    // }
+
+    // opcion 2. mas corta resumida
+    constructor( 
+      public name: string,
+      public gender: Gender,
+      public birthDay: Date,
+    ){} 
+  }
+
+  // const newPerson = new Person('Jeancarlos', 'M', new Date('1984-05-09')); 
+  // console.log(newPerson);
+
+  class User extends Person {
+    
+    private lastAccess: Date;
+    
+    constructor(
+      public email: string,
+      public role: string,
+      name: string,
+      gender: Gender,
+      birthDay: Date,
+    ){
+      // el super llama al constructor del padre (Person)
+      // aqui debo cumplir todo el requisito del constructor del padre
+      super(name, gender, birthDay);
+      this.lastAccess = new Date();
+    }
+
+    checkCredential(){
+      return true;
+    }
+  }
+
+  class UserSettings extends User {
+    constructor(
+      public workingDirectory : string,
+      public lastOpneFolder   : string,
+      email                   : string,
+      role                    : string,
+      name                    : string,
+      gender                  : Gender,
+      birthDay                : Date,
+    ){
+      super(email, role, name, gender, birthDay);
+    }
+  }
+
+  // Esta clase viola el princio de eresponsabilidad Unica
+  const newUser = new UserSettings(
+    'user/home/',
+    'home',
+    'jeancarloscarvajalavila@gmail.com',
+    'Admin',
+    'Jeancarlos',
+    'M',
+    new Date('1984-05-09')
+  );
+
+  console.log( newUser );
+  
+  
+
+})();
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css';
-import './clean-code/05-dry';  
+import './clean-code/06-clases-a.ts';  
+import './test.js';  
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
